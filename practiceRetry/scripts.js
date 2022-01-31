cdb586fa033672f4214fc5862f2424d8114f7059
ErikSionov <ErikSionov@gmail.com>
2022-01-31 17:31:28 +0200

    javascript class work

--- practiceRetry/scripts.js
+++ practiceRetry/scripts.js
@@ -1,6 +1,24 @@
- function endDrag(){
-            console.log("drag ended");
-        }
-        function onDragStart(){
-            console.log("drag started");
-        }
+let pressed;
+
+function endDrag()
+{
+    console.log("drag ended");
+}
+
+function onDragStart()
+{
+    console.log("drag started");
+}
+
+function showMenu()
+{
+    if (!pressed)
+    {
+        pressed = true;
+        document.getElementById("divMenu").style.display = "none";
+    } else
+    {
+        document.getElementById("divMenu").style.display = "block";
+        pressed = false;
+    }
+}
